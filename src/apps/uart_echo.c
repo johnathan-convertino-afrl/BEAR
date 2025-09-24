d750c18ad5cf6cd3c18f185a94a6b1246aa63407
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-09-24 13:23:59 -0400

    add asm comment block for future inline version of the program

--- src/apps/uart_echo.c
+++ src/apps/uart_echo.c
@@ -9,7 +9,16 @@ int main()
   struct s_uart *p_uart = initUart(UART_ADDR);
 
   __delay_ms(2);
-
+  
+//  Inline assembly example of how to print an A on UART.
+//   __asm__ volatile ("andi t3, zero, 0");
+//   
+//   __asm__ volatile ("lui t3, 0x44A10");
+//   
+//   __asm__ volatile ("li t2, 0x41");
+//   
+//   __asm__ volatile ("sw t2,4(t3)");
+  
   for(;;)
   {
     __delay_ms(2);
